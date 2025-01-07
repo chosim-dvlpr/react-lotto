b9527301b3b417ecc9728a39604000624640709a
bmj0053 <bmj05378@gmail.com>
2025-01-08 01:43:20 +0900

    fix: 전역 스타일 border-box 추가 및 backdrop 세로길이 수정

--- src/components/Modal/Modal.styled.tsx
+++ src/components/Modal/Modal.styled.tsx
@@ -19,12 +19,12 @@ export const ContentWrapper = styled.div`
 `;
 
 export const Backdrop = styled.div`
-  position: absolute;
+  position: fixed;
   top: 0;
   left: 0;
 
   width: 100%;
-  height: 100%;
+  height: 100vh;
 
   background-color: rgba(0, 0, 0, 0.5);
 `;
